19d0c8dd480508b4dde7da0d8b67b8bab6b47082
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2017-01-05 19:01:16 +0000

    ...

--- src/lib.rs
+++ src/lib.rs
@@ -24,14 +24,13 @@ impl LPattern {
         let mut captures: Vec<String> = Vec::new();
         let mut fcaptures: Vec<String> = Vec::new();
         for g in &self.groups {
-            println!("Group is {:?}", g);
             let mut substring: Vec<String> = Vec::new();
             for _ in 0..g.len() {
                 println!("Index is {}", i);
                 substring.push(format!("{}", text.chars().nth(i).unwrap()));
                 i += 1;
             }
-            println!("{}", substring.join(""));
+            captures.push(substring.join(""));
         }
         captures
     }
