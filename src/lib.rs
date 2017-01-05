41920848cc6af93461829feb1ff0e54ff1f06f89
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2017-01-05 16:55:04 +0000

    ...

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,7 @@ pub struct LPattern;
 impl LPattern {
     pub fn new(pattern: &str) {
         let mut split = pattern.split(")");
-        for s in split {
-            println!("{}", s);
-        }
+        let groups: Vec<&str> = split.collect();
+        println!("{:?}", groups);
     }
 }
