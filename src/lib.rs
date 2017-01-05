7b30ef2d0abb060782490e6ed8ff14db867bb898
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2017-01-05 17:43:53 +0000

    ...

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ impl LPattern {
         }
     }
 
-    pub fn apply_to(&self, text: &str) {
+    pub fn apply_to(&self, text: &str) -> Vec<String> {
         println!("Appying to pattern: {:?}", self.groups);
         for g in &self.groups {
             print!("{:?}", g.len());
