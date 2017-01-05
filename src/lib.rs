4388eede960c32bdfeb9522cdb203b440b5f12f3
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2017-01-05 18:12:24 +0000

    ...

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub struct LPattern {
 
 impl LPattern {
     pub fn new(pattern: &str) -> LPattern {
-        let mut split = pattern.split("%");
+        let split = pattern.split("%");
         let mut groups: Vec<String> = Vec::new();
         for s in split {
             if s.to_owned().len() > 0 {
@@ -19,11 +19,15 @@ impl LPattern {
 
     pub fn apply_to(&self, text: &str) -> Vec<String> {
         println!("Applying to pattern: {:?}", self.groups);
+        let mut chars: Vec<char> = Vec::new();
+        for c in text.chars() {
+            chars.push(c);
+        }
         let mut captures: Vec<String> = Vec::new();
         for g in &self.groups {
             let mut substring: Vec<String> = Vec::new();
             for i in 0..g.len() - 1 {
-                substring.push(text.chars()[i]);
+                substring.push(format!("{}", chars[i]));
             }
             captures.push(substring.join(""))
         }
